b7e4f380369121629dad049433699aafcc16f1a4
amber227 <jyulin2000@protonmail.com>
2025-01-31 16:26:48 -0600

    +nix: more keybinds

--- +nix.el
+++ +nix.el
@@ -5,7 +5,8 @@
 
 (map! :leader
   "n r" #'nix-reload-system-flake
-  "n p" #'nix-find-file-in-system-flake)
+  "n p" #'nix-find-file-in-system-flake
+  "n d" #'nix-garbage-collect)
 
 (defun nix-find-file-in-system-flake ()
   (interactive)
@@ -14,3 +15,7 @@
 (defun nix-reload-system-flake ()
   (interactive)
   (compile (concat "sudo nixos-rebuild switch --flake "  nix-system-flake-path) t))
+
+(defun nix-garbage-collect ()
+  (interactive)
+  (compile "sudo nix-collect-garbage -d" t))
